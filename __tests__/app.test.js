98102cc4a9aad8e2837f5d677e301ee16bfdf228
Jay <jiyoung26091212@gmail.com>
2022-03-28 15:39:51 +0100

    test : endpoint /api/topics GET 200 response

---
+++ __tests__/app.test.js
@@ -0,0 +1,29 @@
+const db = require('../db/connection.js');
+const app = require('../app');
+const request = require('supertest');
+const seed = require('../db/seeds/seed');
+const testData = require('../db/data/test-data');
+
+beforeEach(() => {
+	return seed(testData);
+});
+afterAll(() => {
+	return db.end();
+});
+
+describe('topics', () => {
+	test('200 /api/topics', () => {
+		return request(app)
+			.get('/api/topics')
+			.expect(200)
+			.then((response) => {
+				expect(response.body.topics).toBeInstanceOf(Object);
+				response.body.topics.forEach((topic) => {
+					expect(topic).toMatchObject({
+						slug: expect.any(String),
+						description: expect.any(String),
+					});
+				});
+			});
+	});
+});
