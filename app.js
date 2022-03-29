f85028368a4f8c8cc75f421ad3d739e20119fb06
Jay <jiyoung26091212@gmail.com>
2022-03-29 11:49:33 +0100

    feat : 500 Error handler to catch unspecified errors

--- app.js
+++ app.js
@@ -10,4 +10,9 @@ app.use((req, res, next) => {
 	res.status(404).send({ msg: 'Not found' });
 });
 
+app.use((err, req, res, next) => {
+	console.log(err);
+	res.status(500).send({ msg: 'Internal server error' });
+});
+
 module.exports = app;
