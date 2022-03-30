9273ec05e9968e737ad0587da545547a45ea3610
Jay <jiyoung26091212@gmail.com>
2022-03-30 13:13:53 +0100

    style : .catch takes a single argument of function. Next itself is a function, and I can make a new  arrow function passing error.

--- models/topics.model.js
+++ models/topics.model.js
@@ -1,7 +1,7 @@
 const db = require('../db/connection');
 
 exports.selectTopics = () => {
-	return db.query('SELECT * FROM topics').then((result) => {
+  return db.query('SELECT * FROM topics;').then((result) => {
 		return result.rows;
 	});
 };
