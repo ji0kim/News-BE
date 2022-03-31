fc7e2b45c0d15735148ac0caa07bf2fc30aaa2d0
Jay <jiyoung26091212@gmail.com>
2022-03-31 14:01:22 +0100

    feat : SELECT all users

--- models/users.model.js
+++ models/users.model.js
@@ -2,9 +2,6 @@ const db = require('../db/connection');
 
 exports.selectUsers = () => {
 	return db.query('SELECT * FROM users').then((result) => {
-		const users = result.rows.map((user) => {
-			return { username: user.username };
-		});
-		return users;
+		return result.rows;
 	});
 };
