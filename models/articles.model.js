9b97bd5798c30dd9827355010867eda42ad1cf0f
Jay <jiyoung26091212@gmail.com>
2022-03-31 15:19:11 +0100

    feat: show comment_count when getting an article by Id

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,5 +1,6 @@
 const db = require('../db/connection');
 const format = require('pg-format');
+const { lastIndexOf } = require('../db/data/test-data/articles');
 
 exports.selectArticles = () => {
 	const queryTxt = 'SELECT * FROM articles';
@@ -10,14 +11,23 @@ exports.selectArticles = () => {
 
 exports.selectArticleById = (article_id) => {
 	const queryTxt = format('SELECT * FROM articles WHERE article_id = $1');
-	return db.query(queryTxt, [article_id]).then((result) => {
-		if (result.rows.length === 0) {
-			return Promise.reject({ status: 404, msg: 'Not found' });
-			// return the promise
-		} else {
-			return result.rows[0];
-		}
-	});
+	const commentCountQueryTxt = format('SELECT * FROM comments WHERE article_id=$1');
+
+	return db
+		.query(queryTxt, [article_id])
+		.then((result) => {
+			if (result.rows.length === 0) {
+				return Promise.reject({ status: 404, msg: 'Not found' });
+			}
+			return result;
+		})
+		.then((result) => {
+			return db.query(commentCountQueryTxt, [article_id]).then((commentById) => {
+				const commentCount = commentById.rows.length;
+				result.rows[0].comment_count = commentCount;
+				return result.rows[0];
+			});
+		});
 };
 
 exports.updateVoteById = (inc_votes, article_id) => {
