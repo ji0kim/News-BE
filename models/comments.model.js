dd033b58b79c6c38f692b7b358009d625db562e0
Jay <jiyoung26091212@gmail.com>
2022-03-31 23:40:36 +0100

    feat: Add 404, 400 error handler for comments

--- models/comments.model.js
+++ models/comments.model.js
@@ -1,9 +1,12 @@
-const res = require('express/lib/response');
 const db = require('../db/connection');
 
 exports.selectCommentsById = (article_id) => {
 	const queryTxt = `SELECT * FROM comments WHERE article_id=$1;`;
 	return db.query(queryTxt, [article_id]).then((result) => {
-		return result.rows;
+		if (result.rows.length === 0) {
+			return Promise.reject({ status: 404, msg: 'Not found' });
+		} else {
+			return result.rows;
+		}
 	});
 };
