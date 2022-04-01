efeb9f2fa1907b8afe2130a9fe8e2c827cb3a1ae
Jay <jiyoung26091212@gmail.com>
2022-04-01 11:50:03 +0100

    refactor : Remove pg-format

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,22 +1,21 @@
 const db = require('../db/connection');
-const format = require('pg-format');
 
 exports.selectArticles = () => {
-  const queryTxt = 'SELECT * FROM articles ORDER BY created_at DESC;';
+	const queryTxt = 'SELECT * FROM articles ORDER BY created_at DESC;';
 	return db.query(queryTxt).then((result) => {
 		return result.rows;
 	});
 };
 
 exports.selectArticleById = (article_id) => {
-  const queryTxt = format(`
+	const queryTxt = `
     SELECT articles.*, COUNT(comment_id)::INT AS comment_count
     FROM articles
     LEFT JOIN comments
     ON articles.article_id = comments.article_id 
     WHERE articles.article_id = $1
     GROUP BY articles.article_id;
-  `);
+  `;
 	return db.query(queryTxt, [article_id]).then((result) => {
 		if (result.rows.length === 0) {
 			return Promise.reject({ status: 404, msg: 'Not found' });
@@ -26,12 +25,12 @@ exports.selectArticleById = (article_id) => {
 };
 
 exports.updateVoteById = (inc_votes, article_id) => {
-  const queryTxt = format(`
+	const queryTxt = `
   UPDATE articles
   SET votes = votes + $1
   WHERE article_id=$2
   RETURNING *;
-    `);
+    `;
 	return db.query(queryTxt, [inc_votes, article_id]).then((result) => {
 		if (result.rows.length === 0) {
 			return Promise.reject({ status: 404, msg: 'Not found' });
