5b8a36ea0a9f17e7aa617e5d7d22ccaf0d46e975
Jay <jiyoung26091212@gmail.com>
2022-04-21 17:35:09 +0100

    feat : Order comments by desc default

--- models/comments.model.js
+++ models/comments.model.js
@@ -1,7 +1,14 @@
 const db = require('../db/connection');
 
-exports.selectCommentsById = (article_id) => {
-  const queryTxt = `SELECT * FROM comments WHERE article_id=$1;`;
+exports.selectCommentsById = (article_id, order = 'desc', sort_by = 'created_at') => {
+	const lowerCaseOrder = order.toLowerCase();
+	const isValidSortByColumn = ['created_at', 'votes'].includes(sort_by);
+	const isValidOrder = ['asc', 'desc'].includes(lowerCaseOrder);
+
+	if (!isValidSortByColumn) return Promise.reject({ status: 400, msg: 'Invalid sort by query' });
+	if (!isValidOrder) return Promise.reject({ status: 400, msg: 'Invalid order query' });
+
+	const queryTxt = `SELECT * FROM comments WHERE article_id=$1 ORDER BY ${sort_by} ${order}`;
 	return db.query(queryTxt, [article_id]).then((result) => {
 		if (result.rows.length === 0) {
 			return Promise.reject({ status: 404, msg: 'Not found' });
